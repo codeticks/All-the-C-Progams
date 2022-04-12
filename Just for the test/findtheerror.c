3595f28104b97dfe042624f952c8e1b80eb4016d
itsmek <yyooggeesshh0011@gmail.com>
2022-04-12 21:47:44 +0530

    Delted all

--- Just for the test/findtheerror.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    int a=9,b=9; float y=2.0;
-    a= b / a;
-    printf("%d",a);
-    
-    
-    return 0;
-}
--- Just for the test/findtheerror2.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(){
-    int marks; float avg;
-    scanf ("%d %f", & marks, & avg);
-    printf("marks are: %d, Avg is %f",marks, avg);
-    return 0;
-}
